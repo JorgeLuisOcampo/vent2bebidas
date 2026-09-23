b23df65fdc3a94256a7526a3849cad2e713925a1
OneHit27 <juandatorres18@gmail.com>
2026-09-23 17:47:13 -0500

    test: crear clase base EnvaseTest para el agregado Envase

---
+++ backend/src/test/java/com/uniquindio/vent2bebidas/domain/entity/EnvaseTest.java
@@ -0,0 +1,16 @@
+package com.uniquindio.vent2bebidas.domain.entity;
+
+import com.uniquindio.vent2bebidas.domain.exception.ReglaDominioException;
+import com.uniquindio.vent2bebidas.domain.valueobject.Capacidad;
+import com.uniquindio.vent2bebidas.domain.valueobject.EstadoEnvase;
+import com.uniquindio.vent2bebidas.domain.valueobject.MaterialEnvase;
+import com.uniquindio.vent2bebidas.domain.valueobject.TipoEnvase;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EnvaseTest {
+    
+}
