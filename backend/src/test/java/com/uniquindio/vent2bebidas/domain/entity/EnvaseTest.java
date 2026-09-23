1cbc1b8617329b0f5e4dce1cd03f9eb16f0f700d
OneHit27 <juandatorres18@gmail.com>
2026-09-23 18:08:33 -0500

    test: validar rechazo al retornar envase no retornable

--- backend/src/test/java/com/uniquindio/vent2bebidas/domain/entity/EnvaseTest.java
+++ backend/src/test/java/com/uniquindio/vent2bebidas/domain/entity/EnvaseTest.java
@@ -12,5 +12,18 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EnvaseTest {
-    
+    @Test
+    void noDebeRetornarEnvaseNoRetornable() {
+
+        Capacidad capacidad = new Capacidad(500);
+        TipoEnvase tipoNoRetornable = new TipoEnvase(MaterialEnvase.PLASTICO, capacidad, false);
+        Envase envase = new Envase(UUID.randomUUID(), tipoNoRetornable);
+
+        ReglaDominioException excepcionCapturada = assertThrows(
+                ReglaDominioException.class,
+                () -> envase.registrarRetorno()
+        );
+
+        assertEquals("El envase no es retornable.", excepcionCapturada.getMessage());
+    }
 }
